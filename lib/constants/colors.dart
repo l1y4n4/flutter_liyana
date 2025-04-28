73bf44ff74b7825d835837571ce4eb0baafddba7
liyana <liyana@tess.gg>
2025-04-28 11:56:34 +0800

    create product list screen

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Color (Red Theme)
+  static const Color primaryColor = Color(0xFFE53935);  
+
+  // Secondary Color
+  static const Color secondaryColor = Color(0xFFFF9800);  
+
+  // Background Color
+  static const Color backgroundColor = Color(0xFFF5F5F5);  
+}
---
+++ lib/models/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final int id;
+  final String title;
+  final double price;
+  final double rating;
+
+  Product({required this.id, required this.title, required this.price, required this.rating});
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      price: (json['price'] as num).toDouble(),
+      rating: (json['rating']['rate'] as num).toDouble(),
+    );
+  }
+}
